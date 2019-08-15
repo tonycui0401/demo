3bfd989ad986795be7a5ae5630ef74d887c72df5
tonycui0401 <tonycui0401@gmail.com>
2019-08-15 11:04:08 +0100

    first commit

--- MyCrawler/app.js
+++ MyCrawler/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const execSync = require('child_process').execSync;
+// const execSync = require('child_process').execSync;
 // import { execSync } from 'child_process';  // replace ^ if using ES modules
 
 
@@ -10,8 +10,8 @@ app.get('/', (req, res) => {
 
 	res.send('Hello World!')
 
-	const output = execSync('python3 extracter.py', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('Output was:\n', output);
+// 	const output = execSync('python3 extracter.py', { encoding: 'utf-8' });  // the default is 'buffer'
+// console.log('Output was:\n', output);
 
 })
 
